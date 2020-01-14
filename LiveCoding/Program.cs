195379647710da12134dc6a6ead6aa5664a0ecee
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2020-01-14 10:51:54 +0100

    initial commit

---
+++ LiveCoding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LiveCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Enumerations
+
+            // OrderType
+            // Parsing
+
+            #endregion
+
+            #region Object / Collection Initializers
+
+            // Sensor Initialization
+
+            #endregion
+
+            #region Extension Methods
+
+            // CharUtils.CountChar
+
+            #endregion
+
+        }
+    }
+}
